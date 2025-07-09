7f88e81906e1a38b8b8b61905b781705914e0a28
darksoldier1404 <darksoldier1029@gmail.com>
2025-07-09 16:13:33 +0900

    PlaceholderUtils update

--- src/main/java/com/darksoldier1404/dppc/api/placeholder/PlaceholderUtils.java
+++ src/main/java/com/darksoldier1404/dppc/api/placeholder/PlaceholderUtils.java
@@ -1,14 +1,23 @@
 package com.darksoldier1404.dppc.api.placeholder;
 
+import com.darksoldier1404.dppc.utils.PluginUtil;
+import com.darksoldier1404.dppc.utils.enums.DependPlugin;
 import me.clip.placeholderapi.PlaceholderAPI;
 import org.bukkit.entity.Player;
 
 public class PlaceholderUtils {
     public static String applyPlaceholder(Player p, String context) {
-        return PlaceholderAPI.setPlaceholders(p, context);
+        if (PluginUtil.isDependPluginLoaded(DependPlugin.PlaceholderAPI)) {
+            return PlaceholderAPI.setPlaceholders(p, context);
+        } else {
+            return context;
+        }
     }
 
     public static boolean isRegistered(String identifier) {
+        if (!PluginUtil.isDependPluginLoaded(DependPlugin.PlaceholderAPI)) {
+            return false;
+        }
         return PlaceholderAPI.isRegistered(identifier);
     }
 }
